80f8309ae8ead4d1f78fc046de7e8d43f419589b
Aurelio A <aavelley@jhu.edu>
2025-02-07 13:00:18 -0500

    Implement Seaching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,11 +4,15 @@ import React from "react";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" alt="Game Hub Logo" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
