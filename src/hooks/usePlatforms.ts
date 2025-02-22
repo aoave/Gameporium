ea784cf2c9597c01a189f7ae5d4e2b7328cf00ce
Aurelio A <aavelley@jhu.edu>
2025-02-22 15:24:09 -0500

    Refactoring Remove Duplicate Interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,7 +3,7 @@ import apiClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 
-interface Platform {
+ export interface Platform {
     id: number;
     name: string;
     slug: string;
